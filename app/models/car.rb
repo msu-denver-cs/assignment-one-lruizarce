b02a208213f1bfb6508fe9a8a85145cb39f91ad3
Luis <lruizarc@msudenver.edu>
2019-12-02 18:24:15 -0700

    validation for car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,9 @@ class Car < ApplicationRecord
   belongs_to :make
   has_many :car_parts
   has_many :parts, through: :car_parts
+  validates :make, presence: true
+  validates_associated :parts
+  validates :vin, presence: true, uniqueness: true
+  validates :model, presence: true, length: {in: 2..10}
 
 end
