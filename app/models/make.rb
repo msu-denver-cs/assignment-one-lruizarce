d0abbf0e680f77958241087dd49a4201fbf221d2
Luis <lruizarc@msudenver.edu>
2019-12-02 18:29:09 -0700

    added validations

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,6 @@
 class Make < ApplicationRecord
   has_many :cars
+  validates_associated :cars
+  validates :name, length: {in: 2..10}
+  validates :country, length: {in: 2..10}
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,5 @@
 class Part < ApplicationRecord
   has_many :car_parts
   has_many :cars, through: :car_parts
+  validates :name, length: { in: 2..10 }
 end
